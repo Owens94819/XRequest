e196d6ef145b0c3bd6cfec88b4ccca0905745712
Owens94819 <johnsonmach6@gmail.com>
2023-04-14 12:07:20 +0100

    ...

---
+++ XRequest.js
@@ -0,0 +1,23 @@
+function XRequest(url, foo, blk) {
+  var id = XRequest.resId++;
+  var s = document.createElement("script");
+  blk && (s.blocking = "render");
+  s.src = url;
+  s.src = XRequest.host + "?id=" + id + "&src=" + s.src;
+  document.head.appendChild(s);
+
+  XRequest.res[id] = [foo, s];
+
+  s.onerror = function () {
+    XRequest.resId--;
+    s.remove();
+    foo && foo("",true);
+    delete XRequest.res[id];
+  };
+}
+
+XRequest.errEv = document.createEvent("Event");
+XRequest.errEv.initEvent(name, false, false);
+XRequest.res = {};
+XRequest.resId = 0;
+XRequest.host = "https://io.cyclic.app/XRequest";
